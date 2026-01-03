0f8e28cc4bacab0849cd6d264d78b318218d1588
ZaraKinYu <zaraknyu@gmail.com>
2026-01-04 01:45:08 +0530

    zienthos-web: add zienthos-web import { defineConfig } from "vite"

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/zienthos-web/", // 👈 REQUIRED for GitHub Pages
+
   server: {
     host: "::",
     port: 8080,
